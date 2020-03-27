40a5c7ffde969e31e4644bc4df2de41c22c5c4c3
HiKami172 <seimur172@gmail.com>
2020-03-27 23:00:13 +0300

    second-commit

--- С lab1-1v_29/Source.cpp
+++ С lab1-1v_29/Source.cpp
@@ -10,21 +10,20 @@ int main()
 	printf("Enter circle's radius: ");
 	scanf("%lf", &r);
 
-	double InscrRad = a / (2 * pow(3, 0.5));
-	double SmallTrian = r * 2 * pow(3, 0.5);
+	double InscrRad = a / (2 * pow(3, 0.5)); //radius of circle inscribed in triangle of given side length
+	double SmallTrian = r * 2 * pow(3, 0.5); //side length of triangle, which describes circle of given radius
 
 	if (r > InscrRad)
-		printf("No circles can be cut out!\n");
+		printf("No circles can be cut out!\n"); //given circle is larger than triangle
 	else if (r == InscrRad)
-		printf("Only one circle can be cut out!\n");
+		printf("Only one circle can be cut out!\n"); //given circle is inscribed in triangle
 	else
 	{
 		int i = 2;
-		while (SmallTrian*i <= a)
+		while (SmallTrian*i <= a) //counting, in how many times given triangle's side length is greater than inscribed triangle's side length
 			i++;
-		i--;
-		printf("%d circles can be cut out!\n", (int)pow((float)i, 2));
-
+		i--; //we got number of small triangles, we can place near given triangle's side
+		printf("%d circles can be cut out!\n", i * i); 
 	}
 
 	system("pause");
